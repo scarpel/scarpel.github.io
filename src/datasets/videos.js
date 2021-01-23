985e99ea3364e27ed9e419b94393bfdc047c4e2b
Guilherme <ghs.scarpel@outlook.com>
2021-01-23 19:52:54 -0300

    fresh and clean - i hope so -

--- src/datasets/videos.js
+++ src/datasets/videos.js
@@ -14,22 +14,6 @@ const videos = [
         link: "https://youtu.be/1YfxrK45qSk",
         duration: "00:06",
         tools: ["afterEffects"]
-    },
-    {
-        img: "images/videos/smartContracts.png",
-        name: "Smart Contracts",
-        description: "Um vídeo que editei para um trabalho da faculdade da disciplina de Segurança da Informação sobre Smart Contracts.",
-        link: "https://youtu.be/fe1vkv6mZRk",
-        duration: "23:11",
-        tools: ["afterEffects"]
-    },
-    {
-        img: "images/videos/livroVsSpoilers.jpg",
-        name: "Abertura de Canal",
-        description: "Um abertura que fiz para o meu antigo, e vergonhoso, canal de YouTube sobre livros.",
-        link: "https://youtu.be/1YfxrK45qSk",
-        duration: "00:06",
-        tools: ["afterEffects"]
     }
 ]
 
